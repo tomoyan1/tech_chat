3c78471ca2873e455eca5a8f9bbc886e888e664b
tomoyan1 <133628017+tomoyan1@users.noreply.github.com>
2026-03-25 12:40:38 +0900

    タスク２完了

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,18 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    Question.create(question_params)
+    redirect_to action: :index
+  end
+  
+  private
+  def question_params
+    params.require(:question).permit(:title, :content, :name)
+  end
 end
